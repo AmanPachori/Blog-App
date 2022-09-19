12083efa71a7b7846415fd9a5685794cf9ee85d2
AmanPachori <amangirrajpachori2002@gmail.com>
2022-09-20 00:58:48 +0530

    minor changes

--- src/components/addBlog/addblog.js
+++ src/components/addBlog/addblog.js
@@ -3,33 +3,98 @@ import Navbar from '../Navbar/navbar'
 import {Container,Button,Row,Col} from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
 import '../../App.css';
+import axios from 'axios';
+const url = 'http://localhost:8000/notes/add';
+
 
 const Addblog = () => {
+  const [title, setTitle] = useState("");
+  const [mainContent, setMainContent] = useState("");
+  const [image,setimage] = useState("");
+  const [file,setfile] = useState(null);
+  const [category, setCategory] = useState([]);
+  const [categoryValues, setCategoryValues] = useState("");
+  
+  function encodeImageFileAsURL(element) {
+    console.log(element);
+    var file = element;
+    var reader = new FileReader();
+    reader.onloadend = function () {
+       setimage(reader.result);
+    };
+    reader.readAsDataURL(file);
+    return reader.result;
+  }
+  const userId  = localStorage.getItem("id");
+  const submit = (e) =>{
+    e.preventDefault();
+    console.log(userId);
+    if(!title&&!mainContent&&!image)
+    {
+      alert('Please enter your email and password');
+    }
+    else{
+      axios.post(url,{
+        title,
+        mainContent,
+        image,
+        userId,
+        category
+      })
+      .then((res) => {
+        if(!res.data.success){
+          alert("Please Login again");
+
+        }
+        else{
+          window.location='./dashboard'
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  } 
+
    const [show, setShow] = useState(false);
-   const handleClose = () => setShow(false);
+   const handleClose = () =>{
+    const array = category;
+    array.push(categoryValues);
+    const newarray = Array.from(new Set(array));
+    setCategory(newarray);
+    console.log(category);
+    setCategoryValues("");
+    setShow(false);
+   }
    const handleShow = () => setShow(true);
-   const [title, setTitle] = useState("");
-   const [mainContent, setMainContent] = useState("");
-   const [image,setimage] = useState("");
+   
+   const removeCategory = (ele) =>{
+    const array = category.filter(el=>{
+      return el!= ele
+    })
+    setCategory(array);
+   }
+   const Categories = category.map((e)=>{
+    return (<div className="m-2 category shadow-lg p-2 mb-5 bg-body rounded float-start" >{e} <button className="m-auto" onClick={()=>{removeCategory(e)}} ><img src="https://img.icons8.com/tiny-glyph/16/000000/delete-sign.png"/></button>
+    </div>) 
+     
+   })
 
   return (
     <div>
-      {/* <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Enter Category</Modal.Title>
         </Modal.Header>
         <Modal.Body className='w-100'>
-        <input type="text" className=" form-control" placeholder="Enter Category" aria-label="addTitle" aria-describedby="basic-addon1"/>
+          <input type="text" value={categoryValues} onChange={(e)=>{setCategoryValues(e.target.value)}} placeholder="Enter Category" className='form-control' />
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
           <Button variant="primary" onClick={handleClose}>
             Add Category
           </Button>
         </Modal.Footer>
-      </Modal> */}
+      </Modal>
         <Navbar/>
         <Container className="p-2 text-start">
          
@@ -37,26 +102,25 @@ const Addblog = () => {
         <div className="border rounded addblog p-5">
            <div className="my-2">
               <h5 style={{color:'#fff'}}>Title</h5>
-              <input type="text" className=" form-control" placeholder="Enter title" aria-label="addTitle" aria-describedby="basic-addon1"/>
+              <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className=" form-control" placeholder="Enter title" aria-label="addTitle" aria-describedby="basic-addon1"/>
            </div>
            <div className="my-2">
               <h5 style={{color:'#fff'}}>Text</h5>
-              <textarea type="text" className="textarea form-control" placeholder="Enter text here" aria-label="addTitle" aria-describedby="basic-addon1"/>
+              <textarea type="text" className="textarea form-control" value={mainContent} onChange={(e)=>{setMainContent(e.target.value)}} placeholder="Enter text here" aria-label="addTitle" aria-describedby="basic-addon1"/>
            </div>
            <div className="">
               <label style={{color:'#fff'}} for="formFile" class=" my-1 form-label"><h5>Upload Image</h5></label>
-              <input class="form-control" type="file" id="formFile"/>
+              <input class="form-control" type="file" value={file} id="formFile" onChange={(e)=>{encodeImageFileAsURL(e.target.files[0])}}/>
            </div>
-           {/* <div className="categories p-1">
-           <Button variant="outline-light" className="m-2 p-1 button float-start" >Category 1 <img src="https://img.icons8.com/ios-glyphs/23/FA5252/macos-close.png"/></Button>
-           <Button variant="outline-light" className="m-2 p-1 button float-start" >Category 2 <img src="https://img.icons8.com/ios-glyphs/23/FA5252/macos-close.png"/></Button>
-           
-             
-           </div> */}
+           <div className="categories my-2">
+           {Categories}
+           </div>
            <div className=""> 
-           <Button variant="outline-light" className="m-2 button float-end" >Publish</Button>
-           {/* <Button variant="outline-light" onClick={handleShow} className="m-2 button float-end" >Add Category</Button> */}
+           <Button variant="outline-light" onClick={submit}  className="m-2 button float-end" >Publish</Button>
+           <Button variant="outline-light" onClick={handleShow} className="m-2 button float-end" >Add Category</Button>
            </div>
+
+           
         </div>
         </Container>
     </div>
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,67 +1,129 @@
+import { useState ,useEffect } from "react";
 import React from 'react'
 import Navbar from '../Navbar/navbar'
 import {Container,Card,Button,Row,Col, Nav} from 'react-bootstrap';
 import '../../App.css'
-const dashboard = () => {
-  return (
-    <div>
-        <Navbar/>
-        <Container className="p-2">
-        <h4 style={{color:'#fff'}} className='text-start'>Dashboard</h4>
+import axios from 'axios';
+const url = '';
 
+
+const Dashboard = () => {
+  const [myblogs,setmyBlogs] = useState([]);
+  const userId = localStorage.getItem('id');
+  useEffect(()=>{
+    GetBlogs();
+  },[])
+  
+  const GetBlogs = () =>{
+    axios.get(`http://localhost:8000/notes/get/${userId}`)
+    .then((res)=>{
+      let array =[res.data];
+      setmyBlogs(array);
+      console.log(myblogs);
+    })
+
+    if(myblogs.length > 0)
+    {
+      return (
+        myblogs[0].data.map((e)=>{
+          return(
+            <Col className="blog border-bottom  w-100 col-lg-5 m-2 col-sm-12 text-start">
+            <div className="p-2">
+              <h6 className="fs-5 fw-bold fst-italic">
+                {" "}
+                {e.title}
+              </h6>
+              <img src={e.image} />
+              <p>
+                {e.mainContent}
+              </p>
+              
+            </div>
+          </Col>
+          )
+        })
+      )
+    }
+
+
+  }
+   
+  if(userId)
+  {
+    return (
+      <div>
+        <Navbar />
+        <Container className="p-2">
+          <h4 style={{ color: "#fff" }} className="text-start">
+            Dashboard
+          </h4>
+  
           <Row>
-            <Col className="border rounded col-lg-9 col-sm-7 m-2 p-2">  
-               <Container>
-                  <Row className="mx-auto d-flex justify-content-center">
-                     <Col className="blog border-bottom  w-100 col-lg-5 m-2 col-sm-12 text-start">
-                      <div className="p-2">
-                         <h6 className="fs-5 fw-bold fst-italic"> Title of the blog</h6>
-                           <p>
-                              lorem's ddens in the ckssvdsk dv cdssxk fdjvsfkn iksnjfpsum dolor sit amet, consecteturv lorem ipsum dolor sit amet, consecteturv   lorem ipsum dolor sit amet, consecteturv lorem ipsum vul    sit amet       
-                           </p>
-                           <span className="m-2"><img src="https://img.icons8.com/material-rounded/27/FFFFFF/facebook-like--v1.png"/> 23</span>
-                           <span className="m-2"><img src="https://img.icons8.com/material-rounded/24/FFFFFF/comments--v1.png"/> 23</span>
-                      </div>
-                     </Col>
-                     <Col className="blog border-bottom  w-100 col-lg-5 m-2 col-sm-12 text-start">
-                      <div className="p-2">
-                         <h6 className="fs-5 fw-bold fst-italic"> Title of the blog</h6>
-                           <p>
-                              lorem's ddens in the ckssvdsk dv cdssxk fdjvsfkn iksnjfpsum dolor sit amet, consecteturv lorem ipsum dolor sit amet, consecteturv   lorem ipsum dolor sit amet, consecteturv lorem ipsum vul    sit amet       
-                           </p>
-                           <span className="m-2"><img src="https://img.icons8.com/material-rounded/27/FFFFFF/facebook-like--v1.png"/> 23</span>
-                           <span className="m-2"><img src="https://img.icons8.com/material-rounded/24/FFFFFF/comments--v1.png"/> 23</span>
-                      </div>
-                     </Col>
-                     <Col className="blog border-bottom  w-100 col-lg-5 m-2 col-sm-12 text-start">
-                      <div className="p-2">
-                         <h6 className="fs-5 fw-bold fst-italic"> Title of the blog</h6>
-                           <p>
-                              lorem's ddens in the ckssvdsk dv cdssxk fdjvsfkn iksnjfpsum dolor sit amet, consecteturv lorem ipsum dolor sit amet, consecteturv   lorem ipsum dolor sit amet, consecteturv lorem ipsum vul    sit amet       
-                           </p>
-                           <span className="m-2"><img src="https://img.icons8.com/material-rounded/27/FFFFFF/facebook-like--v1.png"/> 23</span>
-                           <span className="m-2"><img src="https://img.icons8.com/material-rounded/24/FFFFFF/comments--v1.png"/> 23</span>
-                      </div>
-                     </Col>
-                  </Row>
-               </Container>
+            <Col className="border rounded col-lg-9 col-sm-7 m-2 p-2">
+              <Container>
+                <Row className="mx-auto d-flex justify-content-center">
+                  {GetBlogs()}
+                </Row>
+              </Container>
             </Col>
-            <Col className="border text-start stats rounded m-2 p-2">
-               <h4 class="fst-italic p-2">Stats</h4>
-               <div class="m-2">
-                  <h5 className="fst-italic">Total Blogs  - <span className="fst-italic fw-light fs-6">600</span></h5>
-               </div>
-               <div class="m-2">
-                  <h5 className="fst-italic">Total Likes  - <span className="fst-italic fw-light fs-6">600</span></h5>
-               </div>
-               <div class="m-2">
-                  <h5 className="fst-italic">Total Comments  - <span className="fst-italic fw-light fs-6">600</span></h5>
-               </div>
+            <Col className="text-start ">
+              <div className="profile p-2 border rounded m-2">
+                <div className="d-flex justify-content-between">
+                  <h4 className="my-auto fst-italic p-2"> Profile</h4>
+                  <button className='my-auto'><img src="https://img.icons8.com/ios-glyphs/25/FFFFFF/edit--v1.png" /></button>
+                </div>
+                <div class="m-2">
+                  <h5 className="fst-italic">
+                    Name -{" "}
+                    <span className="fst-italic fw-light fs-6">
+                      Lorem someone
+                    </span>
+                  </h5>
+                </div>
+                <div class="m-2">
+                  <h5 className="fst-italic">
+                    Address -{" "}
+                    <span className="fst-italic fw-light fs-6">
+                      Lrgre rggre lorem rrrgorem someone
+                    </span>
+                  </h5>
+                </div>
+              </div>
+              <div className="stats border rounded m-2 p-2">
+                <h4 class="fst-italic p-2">Stats</h4>
+                <div class="m-2">
+                  <h5 className="fst-italic">
+                    Total Blogs -{" "}
+                    <span className="fst-italic fw-light fs-6">600</span>
+                  </h5>
+                </div>
+                <div class="m-2">
+                  <h5 className="fst-italic">
+                    Total Likes -{" "}
+                    <span className="fst-italic fw-light fs-6">600</span>
+                  </h5>
+                </div>
+                <div class="m-2">
+                  <h5 className="fst-italic">
+                    Total Comments -{" "}
+                    <span className="fst-italic fw-light fs-6">600</span>
+                  </h5>
+                </div>
+              </div>
             </Col>
           </Row>
         </Container>
-    </div>
-  )
+      </div>
+    );
+  }
+  else{
+    return (
+      <h4 style={{ color: "#fff" }} className="text-center">
+            Please Log in
+          </h4>
+    )
+  }
+  
 }
 
-export default dashboard
+export default Dashboard
