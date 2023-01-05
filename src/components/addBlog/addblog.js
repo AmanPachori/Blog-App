595f98d15d0f4c5a9e76bec037bbaf27c33d0561
AmanPachori <amangirrajpachori2002@gmail.com>
2023-01-05 16:54:34 +0530

    minor fixes

--- src/components/addBlog/addblog.js
+++ src/components/addBlog/addblog.js
@@ -1,100 +1,101 @@
 import { useEffect, useState } from "react";
-import Navbar from '../Navbar/navbar'
-import {Container,Button,Row,Col} from 'react-bootstrap';
-import Modal from 'react-bootstrap/Modal';
-import '../../App.css';
-import axios from 'axios';
-import { getUser } from '../../data/data';
-const url = 'http://localhost:8000/notes/add';
-const token = localStorage.getItem('jwt');
+import Navbar from "../Navbar/navbar";
+import { Container, Button, Row, Col } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+import "../../App.css";
+import axios from "axios";
+import { getUser } from "../../data/data";
+const url = "http://localhost:8000/notes/add";
+const token = localStorage.getItem("jwt");
 const config = {
-  headers:{
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' +token
-  }
-}
-const userId  = localStorage.getItem("id");
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + token,
+  },
+};
+const userId = localStorage.getItem("id");
 
 const Addblog = () => {
   const [title, setTitle] = useState("");
   const [mainContent, setMainContent] = useState("");
-  const [image,setimage] = useState("");
-  const [file,setfile] = useState(null);
+  const [image, setimage] = useState("");
+  const [file, setfile] = useState(null);
   const [category, setCategory] = useState([]);
   const [categoryValues, setCategoryValues] = useState("");
-  
-    function encodeImageFileAsURL(element) {
-      console.log(element);
-      var file = element;
-      var reader = new FileReader();
-      reader.onloadend = function () {
-        setimage(reader.result);
-      };
-      reader.readAsDataURL(file);
-      return reader.result;
-    }
-  const submit = (e) =>{
+
+  function encodeImageFileAsURL(element) {
+    console.log(element);
+    var file = element;
+    var reader = new FileReader();
+    reader.onloadend = function () {
+      setimage(reader.result);
+    };
+    reader.readAsDataURL(file);
+    return reader.result;
+  }
+  const submit = (e) => {
     e.preventDefault();
     console.log(userId);
-    if(!title&&!mainContent&&!image)
-    {
-      alert('Please enter your email and password');
+    if (!title && !mainContent && !image) {
+      alert("Please enter your email and password");
+    } else {
+      axios
+        .post(
+          url,
+          {
+            title,
+            mainContent,
+            image,
+            userId,
+            category,
+          },
+          config
+        )
+        .then((res) => {
+          if (!res.data.success) {
+            alert("Please Login again");
+          } else {
+            window.location = "./dashboard";
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-    else{
-      axios.post(url,{
-        title,
-        mainContent,
-        image,
-        userId,
-        category
-      },config)
-      .then((res) => {
-        if(!res.data.success){
-          alert("Please Login again");
+  };
 
-        }
-        else{
-          window.location='./dashboard'
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    }
-  } 
+  const [show, setShow] = useState(false);
+  const handleClose = () => {
+    const array = category;
+    array.push(categoryValues);
+    const newarray = Array.from(new Set(array));
+    setCategory(newarray);
+    setCategoryValues("");
+    setShow(false);
+  };
+  const handleShow = () => setShow(true);
 
-   const [show, setShow] = useState(false);
-   const handleClose = () => {
-     const array = category;
-     array.push(categoryValues);
-     const newarray = Array.from(new Set(array));
-     setCategory(newarray);
-     setCategoryValues("");
-     setShow(false);
-   };
-   const handleShow = () => setShow(true);
-   
-   const removeCategory = (ele) =>{
-    const array = category.filter(el=>{
-      return el!= ele
-    })
+  const removeCategory = (ele) => {
+    const array = category.filter((el) => {
+      return el != ele;
+    });
     setCategory(array);
-   }
-   const Categories = category.map((e) => {
-     return (
-       <div className="m-2 category shadow-lg p-2 mb-5 bg-body rounded float-start">
-         {e}{" "}
-         <button
-           className="m-auto"
-           onClick={() => {
-             removeCategory(e);
-           }}
-         >
-           <i class="bi bi-x-circle-fill"></i>
-         </button>
-       </div>
-     );
-   });
+  };
+  const Categories = category.map((e) => {
+    return (
+      <div className="m-2 category shadow-lg p-2 mb-5 bg-body rounded float-start">
+        {e}{" "}
+        <button
+          className="m-auto"
+          onClick={() => {
+            removeCategory(e);
+          }}
+        >
+          <i class="bi bi-x-circle-fill"></i>
+        </button>
+      </div>
+    );
+  });
   return (
     <div>
       <Modal show={show} onHide={handleClose}>
@@ -103,7 +104,14 @@ const Addblog = () => {
         </Modal.Header>
         <Modal.Body className="w-100">
           {/* <input type="text" value={categoryValues} onChange={(e)=>{setCategoryValues(e.target.value)}} placeholder="Enter Category" className='form-control' /> */}
-          <select class="form-select" value={categoryValues} onChange={(e)=>{setCategoryValues(e.target.value)}} aria-label="Default select example">
+          <select
+            class="form-select"
+            value={categoryValues}
+            onChange={(e) => {
+              setCategoryValues(e.target.value);
+            }}
+            aria-label="Default select example"
+          >
             <option selected>Select Category</option>
             <option value="Technology">Technology</option>
             <option value="Data Science">Data Science</option>
@@ -186,6 +194,6 @@ const Addblog = () => {
       </Container>
     </div>
   );
-}
+};
 
-export default Addblog
+export default Addblog;
